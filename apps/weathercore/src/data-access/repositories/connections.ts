d59ec09b0c978c7f11c95860a744d767678a5a69
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-05-11 15:04:26 +0800

    feat: remove unncessary defintiions. Define connectio on service level.

--- apps/weathercore/src/data-access/repositories/connections.ts
+++ apps/weathercore/src/data-access/repositories/connections.ts
@@ -1,15 +1,3 @@
-import { humidity } from "../models/humidity";
-import { temperature } from "../models/temperature";
-import { rainfall } from "../models/rainfall";
-import { uv } from "../models/uv";
-import { stations } from "../models/station";
-import { createDbConnection } from "../connections/connection";
-import { drizzle } from "drizzle-orm/postgres-js";
+import { createDbConnection } from "../connections/connection"
 
-const dbConnection = createDbConnection(5)
-
-export const stationConnection = drizzle(dbConnection, { schema: { stations}})
-export const tempeatureConnection = drizzle(dbConnection, { schema: { temperature}})
-export const rainfallConnection = drizzle(dbConnection, { schema: { rainfall }})
-export const humidityConnection = drizzle(dbConnection, { schema: { humidity }})
-export const uvConnetion = drizzle(dbConnection, { schema: { uv }})
+export const dbConnection = createDbConnection(5)
