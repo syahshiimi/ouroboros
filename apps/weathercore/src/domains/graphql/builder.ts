f71b58990fdc8765cd2e48c7c1680e2a9dea820b
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-05-18 14:58:10 +0800

    fix: the date scale resolver should be datetime instead of just date.

--- apps/weathercore/src/domains/graphql/builder.ts
+++ apps/weathercore/src/domains/graphql/builder.ts
@@ -1,10 +1,10 @@
 import SchemaBuilder from '@pothos/core'
 import type { SchemaType } from './scalars';
-import { DateResolver } from 'graphql-scalars';
+import { DateResolver, DateTimeResolver } from 'graphql-scalars';
 
 const builder = new SchemaBuilder<SchemaType>({ defaultFieldNullability: true })
 
-builder.addScalarType("Date", DateResolver)
+builder.addScalarType("Date", DateTimeResolver)
 builder.queryType({})
 builder.mutationType({})
 
