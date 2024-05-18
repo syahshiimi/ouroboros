8f1508bf9115a1b78aaf320f672196a93a800661
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-05-18 12:12:07 +0800

    feat: create generic drizzleConnection util function to create db and schema connection.

--- apps/weathercore/src/data-access/repositories/connections.ts
+++ apps/weathercore/src/data-access/repositories/connections.ts
@@ -1,3 +1,9 @@
+import { drizzle } from "drizzle-orm/postgres-js"
 import { createDbConnection } from "../connections/connection"
+import type { PgTableWithColumns } from "drizzle-orm/pg-core"
 
-export const dbConnection = createDbConnection(5)
+const dbConnection = createDbConnection(5)
+
+export function drizzleConnection<T extends PgTableWithColumns<any>>(schema: T) {
+  return drizzle(dbConnection, { schema: { schema } })
+}
