12ca19442559cfa9fee1402f0f83781a55b25e15
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-05-21 22:32:38 +0800

    feat: create relations for tables and also add in fetch_job_type_id to reference other tables.

--- apps/weathercore/src/data-access/models/db/fetch-jobs.ts
+++ apps/weathercore/src/data-access/models/db/fetch-jobs.ts
@@ -1,15 +1,49 @@
+import { relations } from "drizzle-orm";
 import { pgTable, primaryKey, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { humidity } from "./humidity";
+import { rainfall } from "./rainfall";
+import { temperature } from "./temperature";
+import { uv } from "./uv";
+
 
 export const fetchJobs = pgTable('fetch_jobs_task', {
-  id: uuid('id'),
+  id: uuid('id').defaultRandom(),
+  fetch_job_type_id: varchar('fetch_job_type_id', { length: 256 }),
   fetch_date: timestamp('fetch_date', { withTimezone: true }),
   fetch_job_start_date: timestamp('fetch_job_start_date', { withTimezone: true }),
   fetch_url: varchar('fetch_url', { length: 256 }),
-  file_path: varchar('file_path', { length: 256 }),
   file_name: varchar('file_name', { length: 256 }),
+  workflow_id: varchar('workflow_id', { length: 256 }),
   created_at: timestamp('created_at').defaultNow()
 }, (table) => {
   return {
-    pk: primaryKey({ columns: [table.id, table.file_name] })
+    pk: primaryKey({ columns: [table.id] })
   }
 })
+
+/**
+ * Declare one-to-many relations here for the fetch jobs task table.
+ *
+ * A fetch jobs task can have relations to many other records in other tables.
+ * We reference those id's in the fetch_jobs_type_id.
+ *
+**/
+export const fetchJobRelations = relations(fetchJobs, ({ one }) => ({
+  humidity_fetch_task: one(humidity, {
+    fields: [fetchJobs.fetch_job_type_id],
+    references: [humidity.id]
+  }),
+  rainfall_fetch_task: one(rainfall, {
+    fields: [fetchJobs.fetch_job_type_id],
+    references: [rainfall.id]
+  }),
+  temperature_fetch_task: one(temperature, {
+    fields: [fetchJobs.fetch_job_type_id],
+    references: [temperature.id]
+  }),
+  uv_fetch_task: one(uv, {
+    fields: [fetchJobs.fetch_job_type_id],
+    references: [uv.id]
+  })
+}))
+
