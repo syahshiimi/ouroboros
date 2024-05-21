7e0889909f55d901f142e771214ff4e8941c5873
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-05-21 22:58:43 +0800

    feat: create uv repository

---
+++ apps/weathercore/src/data-access/repositories/uv/uv-repository.ts
@@ -0,0 +1,43 @@
+import { eq } from "drizzle-orm";
+import { uv } from "../../models/db/uv";
+import type { UV } from "../../models/types";
+import { drizzleConnection } from "../connections";
+
+const connection = drizzleConnection(uv)
+
+export async function findLatestUvReadingById(
+  id: string
+): Promise<UV | undefined | null> {
+  return await connection.query.schema.findFirst({
+    where: eq(uv.id, id)
+  });
+};
+
+export async function upserUvReading(
+  record: UV[]
+): Promise<UV[]> {
+  return await connection.transaction(async (tx) => {
+    return await tx
+      .insert(uv)
+      .values(record)
+      .onConflictDoNothing()
+      .returning();
+  });
+};
+
+export async function deleteUvReadingById(
+  id: Exclude<UV["id"], undefined | null>
+) {
+  const deletedRow = await connection
+    .delete(uv)
+    .where(eq(uv.id, id))
+    .returning();
+
+  return deletedRow.length > 1 ? null : deletedRow[0];
+};
+
+export async function deleteAllUvReadings(): Promise<UV[]> {
+  return connection.delete(uv).returning();
+};
+
+
