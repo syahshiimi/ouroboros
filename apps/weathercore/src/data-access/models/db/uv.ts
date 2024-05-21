984de912672dff845634963dbe6964e2169cff3d
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-05-21 22:51:54 +0800

    feat: relax the primary key constraints.

--- apps/weathercore/src/data-access/models/db/uv.ts
+++ apps/weathercore/src/data-access/models/db/uv.ts
@@ -1,12 +1,8 @@
-import { decimal, pgTable, primaryKey, timestamp, uuid } from "drizzle-orm/pg-core";
+import { decimal, integer, pgTable, primaryKey, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const uv = pgTable('uv', {
-  id: uuid("id").defaultRandom(),
-  uv_index: decimal("uv_index"),
+  id: uuid("id").defaultRandom().primaryKey(),
+  uv_index: integer("uv_index"),
   timestamp: timestamp("timestamp"),
   date_created: timestamp("date_created").defaultNow()
-}, (table) => {
-  return {
-    pk: primaryKey({ columns: [table.id, table.timestamp, table.uv_index] })
-  }
 })
