580c1a8812a58bcda3361ee0dd3417212ab70533
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-05-21 18:52:39 +0800

    feat: use PGtable type instead to avoid any.

--- apps/weathercore/src/data-access/repositories/connections.ts
+++ apps/weathercore/src/data-access/repositories/connections.ts
@@ -1,9 +1,9 @@
 import { drizzle } from "drizzle-orm/postgres-js"
 import { createDbConnection } from "../connections/connection"
-import type { PgTableWithColumns } from "drizzle-orm/pg-core"
+import type { PgTable } from "drizzle-orm/pg-core"
 
 const dbConnection = createDbConnection(5)
 
-export function drizzleConnection<T extends PgTableWithColumns<any>>(schema: T) {
+export function drizzleConnection<T extends PgTable>(schema: T) {
   return drizzle(dbConnection, { schema: { schema } })
 }
