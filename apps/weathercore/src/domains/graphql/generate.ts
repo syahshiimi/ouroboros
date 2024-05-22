4528b0d381dc7f4420c12272658caae8ceb59139
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-05-22 23:26:16 +0800

    feat: weathercore now exports a master graphql schema to a package/weathercore-schema master folder.

---
+++ apps/weathercore/src/domains/graphql/generate.ts
@@ -0,0 +1,15 @@
+import { lexicographicSortSchema, printSchema } from "graphql";
+import { schema } from "./schema";
+import { writeFileSync } from "fs";
+import path from 'path';
+
+
+export const generateSchemaGraphql = async () => {
+  const schemaAsString = printSchema(lexicographicSortSchema(schema))
+  const cwd = process.cwd();
+  const targetPath = path.join(cwd, '..', '..', 'packages', 'weathercore-schema', 'schema.graphql');
+  writeFileSync(targetPath, schemaAsString)
+}
+
+generateSchemaGraphql()
+
