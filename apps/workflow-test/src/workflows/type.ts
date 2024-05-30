88d857472dbb08230c4f464ea88803e1d2ec53cb
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-05-31 00:10:43 +0800

    feat: delete old workflow project.

--- apps/workflow-test/src/workflows/type.ts
+++
@@ -1,6 +0,0 @@
-import { topics } from "./queue"
-
-export type WorkflowInput = {
-    date: string
-    topic:  keyof typeof topics
-  }
--- packages/s3-client/.eslintrc.js
+++ packages/s3-client/.eslintrc.js
@@ -3,6 +3,10 @@ module.exports = {
   extends: ["@ouroboros/eslint-config/library.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: true
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
   },
+  rules: {
+    '@typescript-eslint/no-floating-promises': 'error'
+  }
 };
