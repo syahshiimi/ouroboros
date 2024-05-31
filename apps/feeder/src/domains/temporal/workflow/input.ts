168431c3f3c09c8c10a6f6a516cb72077f96abc9
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-05-31 23:41:47 +0800

    feat(input): increase input details zod validation.

--- apps/feeder/src/domains/temporal/workflow/input.ts
+++ apps/feeder/src/domains/temporal/workflow/input.ts
@@ -1,8 +1,8 @@
 import { z } from "zod"
 
 export const inputSchema = z.object({
-    date: z.string(),
-    topic: z.string()
+    date: z.string().date(),
+    topic: z.string().toLowerCase()
 })
 
 export type FeederDetails = z.infer<typeof inputSchema>
