37c1a1439f73d0e9b642b049ad35880d505a2548
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-05-31 21:44:41 +0800

    fix(weather-types): Do proper module resolution for weather-types project.

--- packages/weather-types/src/_generated_/zod/index.ts
+++
@@ -1,4 +0,0 @@
-export * as ZHumiditySchema from "./humidity.zod";
-export * as ZRainfallSchema from "./rainfall.zod";
-export * as ZTemperatureSchema from "./temperature.zod";
-export * as ZUvSchema from "./uv.zod";
--- packages/weather-types/src/index.ts
+++ packages/weather-types/src/index.ts
@@ -1,1 +1,6 @@
-export * from "./_generated_/zod/index"
+import * as ZHumiditySchema from "./_generated_/zod/humidity.zod.js";
+import * as ZRainfallSchema from "./_generated_/zod/rainfall.zod.js";
+import * as ZTemperatureSchema from "./_generated_/zod/temperature.zod.js";
+import * as ZUvSchema from "./_generated_/zod/uv.zod.js";
+
+export { ZTemperatureSchema, ZHumiditySchema, ZUvSchema, ZRainfallSchema }
