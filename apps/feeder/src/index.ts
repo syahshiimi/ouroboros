48bf1ce1fc99fc84dd0c5c1fa01291175297018b
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-06-02 01:07:44 +0800

    lint: remove unused imports.

--- apps/feeder/src/index.ts
+++ apps/feeder/src/index.ts
@@ -1,5 +1,5 @@
 import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import {  Hono } from 'hono'
 import buckets from './routes/bucket'
 import upload from "./routes/upload";
 import humidity from "./routes/handlers/humidity";
