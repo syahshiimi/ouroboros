9dd5121e7d5fbf50b0a160a835afb97decba55fe
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-06-01 14:01:00 +0800

    fix: constraint the createworkerhandler gneric type parameter to also extend feeder details.

--- apps/feeder/src/routes/binder/createWorkflowHandler.ts
+++ apps/feeder/src/routes/binder/createWorkflowHandler.ts
@@ -1,19 +1,20 @@
-import {Workflow, WorkflowClient} from "@temporalio/client";
-import {nanoid} from "nanoid";
-import {taskQueueName} from "../../domains/temporal/shared/topics";
+import { Workflow, WorkflowClient } from "@temporalio/client";
+import { nanoid } from "nanoid";
+import { taskQueueName } from "../../domains/temporal/shared/topics";
+import { FeederDetails } from "../../domains/temporal/workflow/input";
 
-interface CreateWorkflowHandler<T> {
-    workflowCallback: Workflow,
-    workflowParameters: T
+interface CreateWorkflowHandler<T extends FeederDetails> {
+  workflowCallback: Workflow,
+  workflowParameters: T
 }
 
-export async function createWorkflowHandler<T>({ workflowCallback, workflowParameters}: CreateWorkflowHandler<T>) {
-    const client = new WorkflowClient();
-    const handler = await client.start(workflowCallback, {
-        workflowId: nanoid(),
-        taskQueue: taskQueueName,
-        args: [workflowParameters]
-    })
-    
-    return handler
+export async function createWorkflowHandler<T extends FeederDetails>({ workflowCallback, workflowParameters }: CreateWorkflowHandler<T>) {
+  const client = new WorkflowClient();
+  const handler = await client.start(workflowCallback, {
+    workflowId: workflowParameters?.topic + "-" + nanoid(),
+    taskQueue: taskQueueName,
+    args: [workflowParameters]
+  })
+
+  return handler
 }
