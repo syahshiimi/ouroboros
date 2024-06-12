cc883e131e3008e3585a1c62ba576b961e2c2182
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-06-12 23:32:27 +0800

    feat: refine the generate file to do some logging.

--- apps/weathercore/src/domains/graphql/generate.ts
+++ apps/weathercore/src/domains/graphql/generate.ts
@@ -1,14 +1,25 @@
 import { lexicographicSortSchema, printSchema } from "graphql";
 import { schema } from "./schema";
 import { writeFileSync } from "fs";
-import path from 'path';
+import path from "path";
 
-export const generateSchemaGraphql = async () => {
-  const schemaAsString = printSchema(lexicographicSortSchema(schema))
-  const cwd = process.cwd();
-  const targetPath = path.join(cwd, '..', '..', 'packages', 'weathercore-schema', 'schema.graphql');
-  writeFileSync(targetPath, schemaAsString)
-}
-
-generateSchemaGraphql()
-
+(async () => {
+  try {
+    const schemaAsString = printSchema(lexicographicSortSchema(schema));
+    const cwd = process.cwd();
+    const projectDir = [
+      "..",
+      "..",
+      "packages",
+      "weathercore-schema",
+      "schema.graphql",
+    ];
+    const targetPath = path.join(cwd, ...projectDir);
+    writeFileSync(targetPath, schemaAsString);
+    console.log(
+      `Schema generation with path: ${path.join(...projectDir)} completed successfully.`,
+    );
+  } catch (error) {
+    console.error("Error generating schema:", error);
+  }
+})();
