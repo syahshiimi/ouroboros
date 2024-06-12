07992a3c74df79b82417e452f91536cb0db62071
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-06-13 01:05:30 +0800

    lint: set correct type for unknown object.

--- apps/feeder/src/domains/temporal/shared/request.ts
+++ apps/feeder/src/domains/temporal/shared/request.ts
@@ -1,14 +1,15 @@
-import {GraphQLClient, RequestDocument } from "graphql-request";
+import { GraphQLClient, RequestDocument } from "graphql-request";
 
-export async function requestClient(document:  RequestDocument, variables: {} | undefined) {
-  const endpoint = `http://${process.env.GRAPHQL_ENDPOINT}`
-  const graphqlClient = new GraphQLClient(
-    endpoint,
-    { headers: {
+export async function requestClient(
+  document: RequestDocument,
+  variables: NonNullable<unknown> | undefined,
+) {
+  const endpoint = `http://${process.env.GRAPHQL_ENDPOINT}`;
+  const graphqlClient = new GraphQLClient(endpoint, {
+    headers: {
       "Content-Type": "application/json",
-      },
-    }
-    );
+    },
+  });
 
-  return await graphqlClient.request(document, { ...variables })
+  return await graphqlClient.request(document, { ...variables });
 }
