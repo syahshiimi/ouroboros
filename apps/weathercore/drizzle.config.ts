099a8bf83dd01e8823f10d88ad7ac6763a01293a
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-06-15 01:08:23 +0800

    chore: move up the db port up by 1.

--- apps/weathercore/drizzle.config.ts
+++ apps/weathercore/drizzle.config.ts
@@ -9,6 +9,7 @@ export default {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME!,
+    port: Number(process.env.DB_PORT!)
   },
   verbose: true,
 } satisfies Config;
