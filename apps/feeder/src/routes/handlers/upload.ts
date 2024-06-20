144850d94c356721a889c6732f9dbc73d5da561d
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-06-20 20:02:41 +0800

    chore: format

--- apps/feeder/src/routes/handlers/upload.ts
+++ apps/feeder/src/routes/handlers/upload.ts
@@ -1,22 +1,26 @@
-import {Hono} from "hono";
-import {nanoid} from "nanoid";
-import {R2, S3Service} from "@ouroboros/s3-client";
+import { Hono } from "hono";
+import { nanoid } from "nanoid";
+import { R2, S3Service } from "@ouroboros/s3-client";
 
 const upload = new Hono();
+
 upload.post('/', async (c) => {
-    const { file } = await c.req.parseBody();
-    if (file instanceof File) {
-        const arrayBuffer = await file.arrayBuffer()
-        const buffer = Buffer.from(arrayBuffer)
-        const response = await R2.send(new S3Service.PutObjectCommand({
-            Bucket: `${process.env.R2_BUCKET_NAME}`,
-            Body: buffer,
-            Key: nanoid() + "-" + file.name,
-            ContentType: file.type
-        }))
-        console.log(response)
-    }
-    return c.text('Uploaded!');
+  const { file } = await c.req.parseBody();
+  if (file instanceof File) {
+    const arrayBuffer = await file.arrayBuffer()
+    const buffer = Buffer.from(arrayBuffer)
+    const bucket = new S3Service.PutObjectCommand({
+      Bucket: `${process.env.R2_BUCKET_NAME}`,
+      Body: buffer,
+      Key: nanoid() + "-" + file.name,
+      ContentType: file.type
+    })
+
+    const response = await R2.send(bucket)
+
+    console.log(response)
+  }
+  return c.text('Uploaded!');
 });
 
 export default upload;
