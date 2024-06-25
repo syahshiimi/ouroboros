10c09659cde41cf555bf3af988aa26d6bbe8245c
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-06-25 22:50:27 +0800

    feat: initialisa a worker app.

---
+++ apps/feeder-worker/.eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  extends: ["@ouroboros/eslint-config/library.js"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
+  },
+  rules: {
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-unused-vars': 'error'
+  }
+};
