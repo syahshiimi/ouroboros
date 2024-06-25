a18b9e9fdccc8542bd2a1f53bdccfdb5e11bebaf
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-06-25 22:14:19 +0800

    feat: delete the feeder domains folder.

--- apps/feeder/src/domains/temporal/activities/mutations/station.ts
+++
@@ -1,9 +0,0 @@
-import { ZTemperatureType, ZHumidityType, ZRainfallType } from "@ouroboros/weather-schema";
-import { unwrapStationDTO } from "../../../dto/stations/station.dto.js";
-
-export const mapStations = async (response: ZTemperatureType | ZHumidityType | ZRainfallType) => {
-  const stations = response.metadata.stations.map((station) =>
-    unwrapStationDTO(station)
-  );
-  return stations;
-};
