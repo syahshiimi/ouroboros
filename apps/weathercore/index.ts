e4523a8ac7a904337e2acc2585e2757baee90cbe
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-07-02 01:04:24 +0800

    chore: logging.

--- apps/weathercore/index.ts
+++ apps/weathercore/index.ts
@@ -1,9 +1,11 @@
 import { yoga } from "./src/domains/graphql/yoga";
 
-const port = 3000
-console.log(`Bun GraphQL Yoga server is running at http://localhost:${port}`)
+const port = 3000;
+console.log(
+  `Bun GraphQL Yoga server is running at http://localhost:${port}/graphql`,
+);
 
 Bun.serve({
   fetch: yoga,
-  port: port
-})
+  port: port,
+});
