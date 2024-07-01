f6138fd91f49aaa57dc5ba66ba0c72d760c5f56e
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-07-02 01:04:45 +0800

    feat: support connection injection via DI.

--- apps/weathercore/src/data-access/connections/connection.ts
+++ apps/weathercore/src/data-access/connections/connection.ts
@@ -1,18 +1,20 @@
 import postgres from "postgres";
 
-const DB_USER = process.env.DB_USER
-const DB_HOST = process.env.DB_HOST
-const DB_PORT = process.env.DB_PORT
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_NAME = process.env.DB_NAME
+const DB_USER = process.env.DB_USER;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
 
-const connectionString = `postgresql://${DB_USER!}:${DB_PASSWORD!}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const localDevString = `postgresql://${DB_USER!}:${DB_PASSWORD!}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
-/** 
- * Creates a DB connection to the endpoint.
- * @param [connection=1]  Defines the connections to the DB. For migrations, set to 1.
-**/
-export function createDbConnection(connection: number = 1) {
+/**
+ * Creates a postgres DB connection to the endpoint.
+ **/
+export function createDbConnection(
+  connection: number = 1,
+  connectionString: string = localDevString,
+) {
   return postgres(connectionString, { max: connection });
 }
 
@@ -20,5 +22,5 @@ export function createDbConnection(connection: number = 1) {
  * Exits the DB conenction gracefully.
  */
 export function exitDbConnection() {
-  return createDbConnection().end()
+  return createDbConnection().end();
 }
