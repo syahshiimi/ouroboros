56ee7f968a95b64ce72dea9e4649295289aa07de
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-07-02 01:05:02 +0800

    feat: use DI to inject postgres connection.

--- apps/weathercore/src/data-access/repositories/fetch-jobs/fetch-jobs-repository.ts
+++ apps/weathercore/src/data-access/repositories/fetch-jobs/fetch-jobs-repository.ts
@@ -2,33 +2,34 @@ import { eq } from "drizzle-orm";
 import { fetchJobs } from "../../models/db/fetch-jobs";
 import type { FetchJobs } from "../../models/types";
 import { drizzleConnection } from "../connections";
+import { createDbConnection } from "../../connections/connection.ts";
 
-const connection = drizzleConnection(fetchJobs);
+export const FetchJobsRepository = async (connectionString?: string) => {
+  const dbConnection = createDbConnection(5, connectionString);
+  const connection = drizzleConnection(fetchJobs, dbConnection);
 
-export async function findFetchJobsTaskById(
-  id: Exclude<FetchJobs["id"], undefined | null>,
-) {
-  return await connection.query.schema.findFirst({
-    where: eq(fetchJobs.id, id),
-  });
-}
-
-export async function findFetchJobsTasksByTopic(
-  fetch_job_type_id: Exclude<FetchJobs["topic"], undefined | null>,
-) {
-  return await connection.query.schema.findFirst({
-    where: eq(fetchJobs.topic, fetch_job_type_id),
-  });
-}
-
-export async function upsertFetchJobsTask(
-  fetch_task: FetchJobs[],
-): Promise<FetchJobs[]> {
-  return await connection.transaction(async (tx) => {
-    return tx.insert(fetchJobs).values(fetch_task).returning();
-  });
-}
-
-export async function deleteFetchJobsTask(): Promise<FetchJobs[]> {
-  return connection.delete(fetchJobs);
-}
+  return {
+    async findFetchJobsTaskById(
+      id: Exclude<FetchJobs["id"], undefined | null>,
+    ) {
+      return await connection.query.schema.findFirst({
+        where: eq(fetchJobs.id, id),
+      });
+    },
+    async findFetchJobsTasksByTopic(
+      fetch_job_type_id: Exclude<FetchJobs["topic"], undefined | null>,
+    ) {
+      return await connection.query.schema.findFirst({
+        where: eq(fetchJobs.topic, fetch_job_type_id),
+      });
+    },
+    async upsertFetchJobsTask(fetch_task: FetchJobs[]): Promise<FetchJobs[]> {
+      return await connection.transaction(async (tx) => {
+        return tx.insert(fetchJobs).values(fetch_task).returning();
+      });
+    },
+    async deleteFetchJobsTask(): Promise<FetchJobs[]> {
+      return connection.delete(fetchJobs);
+    },
+  };
+};
--- apps/weathercore/src/domains/graphql/gql/index.ts
+++ apps/weathercore/src/domains/graphql/gql/index.ts
@@ -1,6 +1,6 @@
 export * from "./stations/stations";
-export * from "./temperature/temperature";
-export * from "./humidity/humidity";
-export * from "./rainfall/rainfall";
-export * from "./uv/uv.ts";
+// export * from "./temperature/temperature";
+// export * from "./humidity/humidity";
+// export * from "./rainfall/rainfall";
+// export * from "./uv/uv.ts";
 export * from "./fetch-jobs/fetch-jobs";
