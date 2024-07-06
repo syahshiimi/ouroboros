8473f5c658632fc8a27550f5085914869dfde5ff
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-07-06 17:06:38 +0800

    feat: workflow package should create a connection defualt.

--- packages/workflows/src/domains/temporal/shared/request.ts
+++ packages/workflows/src/domains/temporal/shared/request.ts
@@ -1,10 +1,15 @@
 import { GraphQLClient, RequestDocument } from "graphql-request";
 
+
+const graphqlConnection = () => process.env.NODE_ENV === "production"
+  ? process.env.GRAPHQL_ENDPOINT
+  : 'localhost:3000/graphql'
+
 export async function requestClient(
   document: RequestDocument,
   variables: NonNullable<unknown> | undefined,
 ) {
-  const endpoint = `http://${process.env.GRAPHQL_ENDPOINT}`;
+  const endpoint = `http://${graphqlConnection()}`;
   const graphqlClient = new GraphQLClient(endpoint, {
     headers: {
       "Content-Type": "application/json",
