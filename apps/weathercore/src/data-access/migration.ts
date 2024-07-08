30137439613a528a3d96aae0509f55111855de7b
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-07-08 21:29:16 +0800

    fix: the migration fiel should exit the process once the migration is done.

--- apps/weathercore/src/data-access/migration.ts
+++ apps/weathercore/src/data-access/migration.ts
@@ -1,16 +1,20 @@
-import { migrate } from 'drizzle-orm/postgres-js/migrator';
-import { drizzle } from 'drizzle-orm/postgres-js';
-import { createDbConnection, exitDbConnection } from './connections/connection';
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import { drizzle } from "drizzle-orm/postgres-js";
+import { createDbConnection, exitDbConnection } from "./connections/connection";
 
 export const main = async () => {
-  const sql = drizzle(createDbConnection(1))
-
-  await migrate(sql, {
-    migrationsFolder: "src/data-access/migrations",
-    migrationsTable: 'drizzle_migrations',
-    migrationsSchema: 'public'
-  })
-  await exitDbConnection()
-}
+  const sql = drizzle(createDbConnection(1));
+  try {
+    await migrate(sql, {
+      migrationsFolder: "src/data-access/migrations",
+      migrationsTable: "drizzle_migrations",
+      migrationsSchema: "public",
+    });
+  } finally {
+    await exitDbConnection();
+  }
+};
 
 main()
+  .catch(console.error)
+  .finally(() => process.exit(0));
