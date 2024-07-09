8abb09954ca5021db18e340ce67ef9c5158b7013
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-07-09 23:01:09 +0800

    feat: breakout the timer useEffect into a custom hook.

--- apps/forecaster/src/app/page.tsx
+++ apps/forecaster/src/app/page.tsx
@@ -1,27 +1,22 @@
 "use client";
-import { useState } from "react";
+
 import SG from "../../public/sg_2.svg";
 import { HumidityText } from "../../public/text/humidity.text";
-
-const getCurrentDate = () => {
-  const date = new Date();
-  return date.toTimeString();
-};
+import { useTimerHook } from "@/hooks/useTimerHook";
 
 export default function Home() {
-  const [pageState, setPageState] = useState("Humidity");
-  const [currentDate, setCurrentDate] = useState(getCurrentDate);
+  const date = useTimerHook();
 
   return (
     <main className="bg-black max-w-screen min-h-screen px-4 py-8 flex flex-col">
-      <section className={`flex flex-col gap-4`}>
-        <h1 className={`text-6xl text-white`}>{pageState} Forecast</h1>
-        <h2 className={`text-xl text-white`}>{currentDate}</h2>
+      <section className={`flex flex-col gap-4 z-20`}>
+        <h1 className={`text-6xl text-h1 text-white`}>Humidity Forecast</h1>
+        <h2 className={`text-body text-white`}>{date}</h2>
       </section>
-      <section className={`flex-grow flex items-center justify-center`}>
+      <section className={`flex-grow flex items-center justify-center z-[-99`}>
         <SG className={`fixed bg-red-700 max-h-[54rem]`} />
       </section>
-      <section className={``}>
+      <section className={`text-furniture`}>
         <p
           className={`fixed left-[30%] bottom-[50%] text-white text-2xl text-nowrap`}
         >
@@ -33,8 +28,8 @@ export default function Home() {
           Sticky.
         </p>
       </section>
-      <section className={`w-full`}>
-        <h2 className={`text-2xl text-white text-center pb-4`}>
+      <section className={`w-full z-20`}>
+        <h2 className={`text-menu text-white text-center pb-4`}>
           {/*I could feel the lick of the wind.*/}
           {HumidityText[2]}
         </h2>
