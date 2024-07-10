e33a3aef8d785bebde2c4121b0fd544c9fa89a93
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-07-11 00:29:52 +0800

    chore: text misses a period.

--- apps/forecaster/public/text/humidity.text.ts
+++ apps/forecaster/public/text/humidity.text.ts
@@ -1,5 +1,5 @@
 export const HumidityText = [
   "I could feel the lick of the wind",
   "You'd imagine it's OK, but you're probably wrong.",
-  "I can guarantee you, with certainty, it will be the same tomorrow",
+  "I can guarantee you, with certainty, it will be the same tomorrow.",
 ];
