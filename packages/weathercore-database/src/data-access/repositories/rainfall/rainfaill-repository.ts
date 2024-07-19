c2ecdd05bc7d4465fa09cdf3332d12de9d809577
syahshiimi <syahrlanuar95@gmail.com>
2024-07-20 00:45:42 +0800

    fix: remove unused await async.

--- packages/weathercore-database/src/data-access/repositories/rainfall/rainfaill-repository.ts
+++ packages/weathercore-database/src/data-access/repositories/rainfall/rainfaill-repository.ts
@@ -8,26 +8,23 @@ export const RainfallRepository = async (connectionString?: string) => {
     connectionString: connectionString,
     schema: rainfall,
   });
-
   return {
     async findLatestRainfallByStationId({
       station_id,
     }: {
       station_id: Exclude<SelectRainfall["station_id"], undefined | null>;
     }): Promise<SelectRainfall | undefined> {
-      return await connection.query.schema.findFirst({
+      return connection.query.schema.findFirst({
         where: eq(rainfall.station_id, station_id),
       });
     },
-
     async findRainfallReadingsByStationId(
       station_id: Exclude<SelectRainfall["station_id"], undefined | null>,
     ): Promise<SelectRainfall[]> {
-      return await connection.query.schema.findMany({
+      return connection.query.schema.findMany({
         where: eq(rainfall.station_id, station_id),
       });
     },
-
     async upsertRainfallReadings(
       record: InsertRainfall[],
     ): Promise<InsertRainfall[]> {
@@ -41,7 +38,7 @@ export const RainfallRepository = async (connectionString?: string) => {
     },
     async deleteRainfallReadingByStationId(
       uuid: Exclude<InsertRainfall["id"], undefined | null>,
-    ) {
+    ): Promise<InsertRainfall[]> {
       return connection
         .delete(rainfall)
         .where(eq(rainfall.id, uuid))
