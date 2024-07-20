59c7352d30f7645575dd57e1fb10cf80d1fe85f7
syahshiimi <syahrlanuar95@gmail.com>
2024-07-20 22:25:24 +0800

    feat: fetch from the API.

--- apps/forecaster/src/app/humidity/page.tsx
+++ apps/forecaster/src/app/humidity/page.tsx
@@ -1,17 +1,26 @@
 "use client";
 
 import SG from "../../../public/sg_2.svg";
-import { HumidityText } from "../../../public/text/humidity.text";
-import { useTimerHook } from "@/hooks/useTimerHook";
+import { useDataHook } from "@/hooks/useDataHook";
+import { getHumidityText } from "@/utils/topics/getHumidity";
+import DateComponent from "@/components/date";
 
-export default function Home() {
-  const date = useTimerHook();
+export default function Humidity() {
+  const { data, isLoading, isError } = useDataHook("humidity");
+
+  if (isLoading) return <p>Retrieving new details</p>;
+  if (isError) return <p>Error fetching data</p>;
+
+  const readings = data.data.items[0].readings;
+  const sum = readings.reduce((acc: any, curr: any) => acc + curr.value, 0);
+  const length = readings.length;
+  const averageHumidity = Math.round(sum / length);
 
   return (
     <main className="bg-black max-w-screen min-h-screen px-4 py-8 flex flex-col">
       <section className={`flex flex-col gap-4 z-20`}>
         <h1 className={`text-h2 text-white`}>Humidity Forecast</h1>
-        <h2 className={`text-body text-white`}>{date}</h2>
+        <DateComponent />
       </section>
       <section
         className={`flex-grow flex text-menu-t items-center justify-center text-white z-[-99`}
@@ -22,13 +31,13 @@ export default function Home() {
             Sticky
           </p>
           <p className={`absolute right-[15%] bottom-[52%] text-nowrap`}>
-            Sticky
+            {averageHumidity}%
           </p>
         </div>
       </section>
       <section className={`w-full z-20`}>
         <h2 className={`text-menu-t text-center pb-4 text-green-200`}>
-          {HumidityText[2]}
+          {getHumidityText(averageHumidity)}
         </h2>
       </section>
     </main>
