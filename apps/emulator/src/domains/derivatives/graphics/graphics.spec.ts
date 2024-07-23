4221169c1d15c753b2d8f73986be653bb7465230
syahshiimi <syahrlanuar95@gmail.com>
2024-07-23 21:46:19 +0800

    chore: shift some stuff around.

--- apps/emulator/src/domains/derivatives/graphics/graphics.spec.ts
+++ apps/emulator/src/domains/derivatives/graphics/graphics.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "vitest";
-import { derivativeGraphics } from "./graphics.ts";
+import { derivativeGraphics } from "./index.ts";
 
 describe("derivativeGraphics", () => {
   // Test that the graphics object has the expected properties
