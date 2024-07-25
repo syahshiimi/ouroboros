e94af84ed87f239f32f5a28697b46dcd8e93c860
syahshiimi <syahrlanuar95@gmail.com>
2024-07-26 00:59:24 +0800

    feat: add merchant emulator.

--- apps/agents-merchants/src/agent.ts
+++ apps/agents-merchants/src/agent.ts
@@ -16,49 +16,28 @@ import { seeder } from "./utils/seeder.ts";
 function agentEmulation() {
   const agentName = faker.company.name();
   const topic = seeder();
-  const derivative = merchant.create(topic).log();
+  const derivative = merchant.create(topic).result;
 
-  merchant.generateContract(derivative).log();
   agents.purchase(agentName);
+  merchant.generateContract(derivative).log();
 
   // This is a representation of the actual derivative asset class.
   // Agents: Visual representation of a _successful_ derivative transaction by agents (Market participants)
-  setTimeout(() => {
-    console.log(derivativeGraphics.getVisuals(generator(0, 3), topic));
-  }, 500);
-
-  // TODO: Might want to remove this part... considering this seems to be the responsibiliyt of the merchant to emit.
-  // This is a representation of the actual contract derivative produced by the merchant.
-  // Merchants: Visual representation of the derivative contracts the merchant has produced.
-  setTimeout(() => {
-    // TODO: Let's have each topic produce three separate graphics.
-    // Therefore, if there are three topics, we should have a total of 9 graphics.
-    console.log(`
-       ☀️   ☁️   ☁️  ☀️
-    __/\\/\\__/\\/\\__/\\/\\__
-     )  ☂️   ☂️   ☂️   (
-    (     HUMIDITY    )
-     )    90% | 90%  (
-    (     80% | 80%   )
-     )    70% | 70%  (
-    (     60% | 60%   )
-     )  WET   | DRY  (
-    (___________________)
-      |  |  |🌡️|  |  |
-      |  |  |35°|  |  |
-    ~~|  |~~|   |~~|  |~~
-    🌿 🌴  🌿 🌴 🌿 🌴 🌿
-
-     ${derivative.location} WEATHER
-       DERIVATIVE
-    `);
-  }, 2000);
+  setTimeout(
+    () => {
+      console.log(derivativeGraphics.getVisuals(generator(0, 3), topic));
+    },
+    generator(500, 2000),
+  );
 }
 
 const main = () => {
-  setInterval(() => {
-    agentEmulation();
-  }, 5000);
+  setInterval(
+    () => {
+      agentEmulation();
+    },
+    generator(5000, 10000),
+  );
 };
 
 main();
