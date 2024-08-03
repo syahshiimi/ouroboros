c5ce9385c9f2f69c873f2f7787445a71fc63cd6e
syahshiimi <syahrlanuar95@gmail.com>
2024-08-04 05:16:34 +0800

    feat: use the status component.

--- apps/forecaster/src/app/temperature/page.tsx
+++ apps/forecaster/src/app/temperature/page.tsx
@@ -5,6 +5,7 @@ import { getTemperatureText } from "@/utils/topics/getTemperature";
 import DateComponent from "@/components/date";
 import { useDataHook } from "@/hooks/useDataHook";
 import { reducer } from "@/utils/reducer";
+import { Status } from "@/components/status";
 
 export default function Home() {
   const { data, isLoading, isError } = useDataHook("temperature");
@@ -22,15 +23,7 @@ export default function Home() {
       <section
         className={`flex-grow flex text-menu-t items-center justify-center text-white z-[-99`}
       >
-        <div className={`h-fit w-fit relative`}>
-          <SG className={`bg-blue-300 min-h-[58rem]`} />
-          <p className={`absolute bottom-[49%] left-[12%] text-nowrap`}>
-            Sticky
-          </p>
-          <p className={`absolute right-[15%] bottom-[52%] text-nowrap`}>
-            Sticky
-          </p>
-        </div>
+        <Status input={averageTemperature} />
       </section>
       <section className={`w-full z-20`}>
         <h2 className={`text-menu-t text-center pb-4 text-green-200`}>
