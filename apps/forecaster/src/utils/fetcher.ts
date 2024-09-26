0b7d3a25dd8b663a009dc7e6996e0cbbac9c47ef
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-09-26 16:03:11 +0800

    feat: set revalidate to zero to prevent caching.

--- apps/forecaster/src/utils/fetcher.ts
+++ apps/forecaster/src/utils/fetcher.ts
@@ -1,7 +1,7 @@
 export const fetcher = async ({url}: {url: string;}) => {
   const options = {
     next: {
-      revalidate: 300
+      revalidate: 0
     }
   };
   const response = await fetch(`${url}`, options);
