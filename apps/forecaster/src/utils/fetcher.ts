deae3d5deab50f3aef277bdec25235626de0d48c
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-09-26 22:19:14 +0800

    feat: let the route handle its own validation

--- apps/forecaster/src/utils/fetcher.ts
+++ apps/forecaster/src/utils/fetcher.ts
@@ -1,12 +1,5 @@
 export const fetcher = async ({url}: {url: string;}) => {
-  const opts = {
-    // next: {
-    //   revalidate: 300
-    // }
-    cache: 'no-store',
-  };
-
-  const response = await fetch(`${url}`, { cache: "no-store"});
+  const response = await fetch(`${url}`);
   if (!response.ok) {
     throw new Error("An error occurred while fetching the data.");
   }
