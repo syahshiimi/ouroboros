bab02af6557f2f54ef39823954e0dcd5b03b4fff
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-09-26 16:57:52 +0800

    feat: give a user date offset

--- apps/forecaster/src/utils/date.ts
+++ apps/forecaster/src/utils/date.ts
@@ -1,6 +1,9 @@
 export const getCurrentDate = () => {
+  // We set a user offset of +8 as the serverless function runs on a UTC+0 offset
+  const userOffset = 8
+
   const now = new Date();
   const offsetMinutes = now.getTimezoneOffset();
-  const adjustedDate = new Date(now.getTime() - offsetMinutes * 60000);
+  const adjustedDate = new Date(now.getTime() - offsetMinutes * 60000 + userOffset);
   return adjustedDate.toISOString().split(".")[0];
 };
