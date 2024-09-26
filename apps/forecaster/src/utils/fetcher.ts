292aab2361bad1b081df46e2d948dd63f2c8802a
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-09-26 15:07:40 +0800

    feat: add next fetch options for vercel deployment.

--- apps/forecaster/src/utils/fetcher.ts
+++ apps/forecaster/src/utils/fetcher.ts
@@ -1,9 +1,10 @@
-export const fetcher = async ({
-  url,
-}: {
-  url: string;
-}) => {
-  const response = await fetch(`${url}`);
+export const fetcher = async ({url}: {url: string;}) => {
+  const options = {
+    next: {
+      revalidate: 300
+    }
+  };
+  const response = await fetch(`${url}`, options);
   if (!response.ok) {
     throw new Error("An error occurred while fetching the data.");
   }
