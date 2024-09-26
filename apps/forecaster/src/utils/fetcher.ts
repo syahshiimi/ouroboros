fb3e8d514011fbd3c3a5fb21f3f1e93176494183
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-09-26 21:08:20 +0800

    fix: disable revalidaet, jsut don't store.

--- apps/forecaster/src/utils/fetcher.ts
+++ apps/forecaster/src/utils/fetcher.ts
@@ -1,10 +1,12 @@
 export const fetcher = async ({url}: {url: string;}) => {
-  const options = {
-    next: {
-      revalidate: 300
-    }
+  const opts = {
+    // next: {
+    //   revalidate: 300
+    // }
+    cache: 'no-store',
   };
-  const response = await fetch(`${url}`, options);
+
+  const response = await fetch(`${url}`, { cache: "no-store"});
   if (!response.ok) {
     throw new Error("An error occurred while fetching the data.");
   }
