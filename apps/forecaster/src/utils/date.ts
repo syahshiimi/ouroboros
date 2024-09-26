401a193409b935a6eef4d4db6e53c9afffa63b84
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-09-26 23:40:13 +0800

    fix: refine the logic to add miliseconds utc+8 offset

--- apps/forecaster/src/utils/date.ts
+++ apps/forecaster/src/utils/date.ts
@@ -1,6 +1,15 @@
 export const getCurrentDate = () => {
   const now = new Date();
-  const offsetMinutes = now.getTimezoneOffset();
-  const adjustedDate = new Date(now.getTime() - offsetMinutes * 60000);
+  const milisecondsPerMinute = 60 * 1000
+  const milisecondsPerHour = 60 * 60 * 1000
+
+  // Convert local time to UTC by adding tz offset.
+  const utc = now.getTime() + (now.getTimezoneOffset() * milisecondsPerMinute);
+
+  // Desired offset of Asia/Singapore which is UTC+8.
+  const offsetHours = 8;
+
+  // Add the 8 hours as miliseconds to offsetHours
+  const adjustedDate = new Date(utc + (milisecondsPerHour* offsetHours));
   return adjustedDate.toISOString().split(".")[0];
 };
