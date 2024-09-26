a4d48fbffbe205cf733f90ebdd5a7f8c3ad40ecb
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-09-26 22:29:21 +0800

    feat: add reading timestamp

--- apps/forecaster/src/app/humidity/page.tsx
+++ apps/forecaster/src/app/humidity/page.tsx
@@ -14,10 +14,14 @@ export default function Humidity() {
   if (isError) return <p>Error fetching data</p>;
 
   const readings = data.data.data.readings[0].data;
+  const readingTimestamp = data.data.data.readings[0].timestamp;
   const averageHumidity = parseFloat(reducer(readings).toFixed(1));
 
   return (
     <main className="bg-black max-w-screen min-h-screen px-4 py-8 flex flex-col">
+      <p className="text-[12px] text-white">
+      Reading timestamp: {readingTimestamp}
+      </p>
       <section className={`flex flex-col gap-4 z-20`}>
         <h1 className={`text-h2 text-white`}>Humidity Forecast</h1>
         <DateComponent />
