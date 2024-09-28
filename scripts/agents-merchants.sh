721ede0acdaed9dcb9318c4d718cd7793dd3c7af
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-09-28 17:16:29 +0800

    feat: use xdo tool to emulate keys

--- scripts/agents-merchants.sh
+++ scripts/agents-merchants.sh
@@ -5,6 +5,7 @@
 run_commands() {
   cd agents-merchants || return
   docker compose up
+  # keep the terminal open
   exec bash
 }
 
@@ -12,4 +13,15 @@ run_commands() {
 export -f run_commands
 
 # Open mate-terminal in full screen and run our commands
-mate-terminal --maximize --command="bash -c run_commands"
+mate-terminal --maximize --command="bash -c run_commands" &
+
+# Wait for terminal to open
+sleep 5
+
+# Use xdotool to find the opened mate-terminal
+WINDOW_ID=$(xdotool search --pid $! --class "mate-terminal" | tail -n1)
+
+# Activate the window and send F11 to toggle full-screen
+xdotool windowactivate $WINDOW_ID
+xdotool key F11
+
