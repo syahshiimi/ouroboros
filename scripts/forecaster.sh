a07e5e1976c1ebba74538bad1cab015e891f6038
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-09-29 23:16:57 +0800

    feat: use xset to defer screen going blank

--- scripts/forecaster.sh
+++ scripts/forecaster.sh
@@ -2,6 +2,10 @@
 
 sleep 10
 
+# Disable xset blankingx
+xset s noblank
+xset s off
+
 # Hide the mouse cursor.
 unclutter -idle 1 -root &
 
@@ -9,5 +13,5 @@ unclutter -idle 1 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' '~/.config/chromium/Default/Preferences'
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' '~/.config/chromium/Default/Preferences'
 
-# Start Chromium.
+#firefox --kiosk 'https://ouroboros-forecaster.vercel.app/rainfall' &
 chromium-browser --kiosk --noerrdialogs --disable-infobars 'https://ouroboros-forecaster.vercel.app/rainfall' &
