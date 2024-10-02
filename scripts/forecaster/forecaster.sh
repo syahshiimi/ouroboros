b31f5f592c839a2e41197b51865764a9141be9e3
Syahrul Anuar <71956472+syahshiimi@users.noreply.github.com>
2024-10-02 23:39:27 +0800

    fix: use chromium and remove unclutter by moving it to a separate script

--- scripts/forecaster/forecaster.sh
+++ scripts/forecaster/forecaster.sh
@@ -6,12 +6,8 @@ sleep 10
 xset s noblank
 xset s off
 
-# Hide the mouse cursor.
-unclutter -idle 1 -root &
-
 # Let Chromium think it always exited cleanly.
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' '~/.config/chromium/Default/Preferences'
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' '~/.config/chromium/Default/Preferences'
 
-#firefox --kiosk 'https://ouroboros-forecaster.vercel.app/rainfall' &
-chromium-browser --kiosk --noerrdialogs --disable-infobars 'https://ouroboros-forecaster.vercel.app/rainfall' &
+chromium-browser --kiosk --start-maximized --noerrdialogs --disable-infobars 'https://ouroboros-forecaster.vercel.app/rainfall'
